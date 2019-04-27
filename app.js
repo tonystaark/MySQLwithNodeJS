83387c55b898f9840bef48a02c9511c4c4bc5d3e
Tony Ng <ngkokhong.tony@gmail.com>
2019-04-27 12:40:30 +0800

    remove unused

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const database = require('./database.js');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -30,22 +29,6 @@ app.use('/api/retrievefornotifications', notificationRouter);
 
 global.isEmptyObject = globalVar.isEmptyObject
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  next()
-});
 
 process.env.NODE_ENV !== 'development' ? port = 3001 : port = 4001;
 server.listen(port, function () {
